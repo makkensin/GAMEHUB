90da4af2400a8b6c7eac4900ac022933ba66c0e9
makkensin <145702858+makkensin@users.noreply.github.com>
2024-07-04 03:29:36 +0200

    FEAT inputname and problems with border radius green

---
+++ SRC/COMPONENTS/inputName.js
@@ -0,0 +1,40 @@
+
+
+
+import './inputName.css'
+import { printName } from './printName';
+
+export const inputName = () =>{
+ console.log('hola como estas?');
+
+ const inputOk = document.querySelector('.inputok')
+ inputOk.innerHTML = '';
+
+ const section = document.querySelector('.pregame');
+
+ const inputName = document.createElement('div');
+ const player = document.createElement('div');
+ const label2 = document.createElement('label');
+ label2.textContent = 'Your Name';
+ const input = document.createElement('input');
+ const button = document.createElement('button');
+ button.textContent = 'JUEGA';
+
+ inputName.classList = 'inputname';
+ player.classList = 'player';
+ input.id = 'input';
+ button.id = 'button';
+
+ button.addEventListener('click', () => {
+  const namePlayer = input.value; 
+  printName(namePlayer)
+  
+});
+player.append(label2,input,button);
+inputName.append(player);
+section.append(inputName);
+}
+
+
+
+
--- SRC/COMPONENTS/introPassword.js
+++ SRC/COMPONENTS/introPassword.js
@@ -1,34 +1,36 @@
 
 
+
 import './introPassword.css'
+import { inputName } from './inputName';
 
 export const introPassword = (password) =>{
-  console.log('adios');
-  console.log(password);
   const section = document.querySelector('.pregame');
   const inputPassword = document.createElement('div');
   const name = document.createElement('div');
   const label = document.createElement('label');
-  label.textContent = 'recuerda';
+  label.textContent = 'Recuerda';
   const input = document.createElement('input');
   const button = document.createElement('button');
-  button.textContent = 'enviar';
-  
-  button.addEventListener('click', () => {
-    const inputValue = input.value; 
-    if (password === inputValue) {
-      alert('correcto');
-    } else {
-        alert('intenta otra vez')
-    }
-});
-
+  button.textContent = 'ENVIAR';
   
   inputPassword.classList = 'inputpassword';
   name.classList = 'name';
   input.id = 'input';
   button.id = 'button';
 
+  button.addEventListener('click', () => {
+    const inputValue = input.value; 
+    if (password === inputValue) {
+      inputPassword.classList.remove('inputpassword')
+      inputPassword.classList.add('inputok')
+      setTimeout(inputName, 3000)
+    } else {
+      inputPassword.classList.remove('inputpassword')
+      inputPassword.classList.add('inputwrong')
+        
+    }
+});
 name.append(label,input,button);
 inputPassword.append(name);
 section.append(inputPassword);
