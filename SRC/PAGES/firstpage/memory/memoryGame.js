c3d6af3d5ad02e98832e7920656c80a14c792210
makkensin <145702858+makkensin@users.noreply.github.com>
2024-09-01 01:41:03 +0200

    FIXED memorygame funtion

--- SRC/PAGES/firstpage/memory/memoryGame.js
+++ SRC/PAGES/firstpage/memory/memoryGame.js
@@ -3,49 +3,73 @@
 import { dataMemoryCards } from '../../../DATA/dataMemory';
 import './memoryGame.css'
 
-export const memoryGame = (element) =>{
 
-  const runeSection = document.querySelector('.runesection', '.runesection__transparent');
+export const memoryGame = (element) => {
+
+  const runeSection = document.querySelector('.runesection'); 
   
   const memorySection1 = document.createElement('section');
-  memorySection1.classList = 'memorysection1';
+  memorySection1.classList.add('memorysection1');
 
-  const fragment1 = document.createDocumentFragment();
-      const shuffledCards1 = dataMemoryCards.sort(() => 0.5 - Math.random());
-      
-      for (const card of shuffledCards1) {
-        const cardRune1 = document.createElement('img');
-        cardRune1.classList = 'cardrune1';
-          cardRune1.src = card;
-          fragment1.append(cardRune1);
-        setTimeout(() => {
-          memorySection1.append(fragment1);
-        }, 2000);
-      }
-  
+  const shuffledCards1 = dataMemoryCards.sort(() => 0.5 - Math.random());
+
+   for (const card of shuffledCards1) {
+
+     const container = document.createElement('div');
+     container.classList = 'container';
+      const card1 = document.createElement('div');
+      card1.classList = 'card1';
+        const cardFront = document.createElement('div');
+        cardFront.classList = 'card-front';
+        const cardBack = document.createElement('div');
+        cardBack.classList = 'card-back';
+
+        const imgFront = document.createElement('img');
+        imgFront.src = './witcher.jpeg';
+        const imgBack = document.createElement('img');
+        imgBack.src = card;
+
+        cardFront.append(imgFront);
+        cardBack.append(imgBack);
+
+        card1.append(cardFront,cardBack);
+        container.append(card1);
+      memorySection1.append(container);
+   }
 
    const memorySection2 = document.createElement('section');
-   memorySection2.classList = 'memorysection2';
-   const fragment2 = document.createDocumentFragment();
-   
-        const shuffledCards2 = dataMemoryCards.sort(() => 0.5 - Math.random());
-        
-        for (const card of shuffledCards2) {
-        const cardRune2 = document.createElement('img');
-        cardRune2.classList = 'cardrune1';
-          cardRune2.src = card;
-          fragment2.append(cardRune2);
-          setTimeout(() => {
-            memorySection2.append(fragment2);
-          }, 4000);
-        }
-      
-   
+   memorySection2.classList.add('memorysection2');
  
-
-
+   const shuffledCards2 = dataMemoryCards.sort(() => 0.5 - Math.random());
    
-runeSection.append(memorySection1, memorySection2);
+   for (const card of shuffledCards2) {
 
-  
+    const container2 = document.createElement('div');
+    container2.classList = 'container2';
+     const card2 = document.createElement('div');
+     card2.classList = 'card2';
+       const cardFront2 = document.createElement('div');
+       cardFront2.classList = 'card-front2';
+       const cardBack2 = document.createElement('div');
+       cardBack2.classList = 'card-back2';
+
+       const imgFront2 = document.createElement('img');
+       imgFront2.src = './witcher.jpeg';
+       const imgBack2 = document.createElement('img');
+       imgBack2.src = card;
+
+       cardFront2.append(imgFront2);
+       cardBack2.append(imgBack2);
+
+       card2.append(cardFront2,cardBack2);
+       container2.append(card2);
+     memorySection2.append(container2);
+  }
+  runeSection.append(memorySection1, memorySection2);
 }
+
+
+
+
+
+ 
